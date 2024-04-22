3f667ec9a795a7eb402f2f64988e87e0fba7ca31
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-04-22 17:03:05 +0300

    Initial Commit

---
+++ App485/App485App.swift
@@ -0,0 +1,17 @@
+//
+//  App485App.swift
+//  App485
+//
+//  Created by IGOR on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct App485App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App485/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App485
+//
+//  Created by IGOR on 22/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
